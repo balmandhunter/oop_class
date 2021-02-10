f031760ac97c234d1dcc42eb4df9b6c3f7b16dbe
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-02-09 22:07:22 -0500

    finish assignment

--- hw1/main.py
+++ hw1/main.py
@@ -1,73 +1,76 @@
-import pandas as pd
-import tkinter as tk
-from tkinter import ttk
-from tkinter import *
-import textwrap
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    import pandas as pd
+    from tkinter import ttk
+    from tkinter import *
+    import tkinter as tk
+    import textwrap
+
     # --- functions ---
 
     df2 = None
-    table = None
     current_index = None
 
+    def import_data():
+        global df
+        df = pd.read_csv("data/IMDB-Movie-Data.csv", index_col=None)
+        df.rename(columns={"Runtime (Minutes)": "Runtime (min)",
+                           "Revenue (Millions)": "Revenue (mil.)"}, inplace=True)
 
-    # a function we call to display pandas data
-    def showdata():
-        # create Treeview with a column for each movie atrribute
-        cols = list(df2.columns)
-        treeview = ttk.Treeview(mainframe, columns=cols, show='headings', height=len(df2))
-        treeview.grid(column=0, row=1, padx=5, sticky=(E, W))
+        return df
 
-        for i in cols:
-            treeview.column(i, anchor="w")
-            treeview.heading(i, text=i, anchor='w')
+    # write a function to wrap text
+    def wrap(string, length=25):
+        return '\n'.join(textwrap.wrap(string, length))
 
+    # populate the table
+    def populate_table(results_table, cols):
+        # setup columns and headings
+        for col in cols:
+            if col in ["Rank", "Year", "Rating"]:
+                results_table.column(col, width=40, anchor="w")
+            elif col == "Votes":
+                results_table.column(col, width=50, anchor="w")
+            elif col in ["Runtime (min)", "Revenue (mil.)", "Metascore"]:
+                results_table.column(col, width=80, anchor="w")
+            else:
+                results_table.column(col, anchor="w")
+            results_table.heading(col, text=col, anchor="w")
+
+        # populate the table
         for index, row in df2.iterrows():
-            treeview.insert("", 0, text=index, values=list(row))
+            results_table.insert("", 0, text=index, values=[wrap(str(x)) for x in list(row)])
 
-        treeview.column("Rank", width=50, stretch=NO)
-        treeview.column("Year", width=50, stretch=NO)
-        treeview.column("Rating", width=50, stretch=NO)
-        treeview.column("Votes", width=50, stretch=NO)
-        treeview.column("Title", width=100, stretch=NO)
-        treeview.column("Genre", width=100, stretch=NO)
-        treeview.column("Director", width=100, stretch=NO)
-        treeview.column("Description", width=100, stretch=NO)
-        treeview.column("Actors", width=100, stretch=NO)
-        treeview.column("Runtime (Minutes)", width=110, stretch=NO)
-        treeview.column("Revenue (Millions)", width=110, stretch=NO)
-        treeview.column("Metascore", width=100, stretch=NO)
-
-
-
-
-
-
+    # display pandas data
+    def showdata():
+        # create results_table with a column for each movie attribute
+        cols = list(df2.columns)
+        results_table = ttk.Treeview(frame_data, columns=cols, show="headings", height=13)
+        results_table.grid(column=0, row=0, padx=0, sticky=W)
+        populate_table(results_table, cols)
 
     def on_select(val):
         global df2
         global current_index
 
         val = selected.get()
-        if val == 'all':
+        if val == "all":
             df2 = df
             next_button.grid_forget()
             showdata()
         else:
-            df2 = df[df['Title'] == val]
+            df2 = df[df["Title"] == val]
             # find index
             current_index = 0
             for ind in df.index:
-                if df['Title'][ind] == val:
+                if df["Title"][ind] == val:
                     current_index = ind
 
-            # put next button on the canvas
-            next_button.grid(row=0, column=0)
             showdata()
-
+            # put next button on the canvas
+            next_button.grid(row=2, column=0, sticky=W)
 
     def next_data():
         global current_index
@@ -83,46 +86,41 @@ if __name__ == '__main__':
     frame_data = None
 
     # import the data
-    df = pd.read_csv('data/IMDB-Movie-Data.csv', index_col=None)
+    df = import_data()
 
     # setup and name the window
     root = tk.Tk()
-    root.title("Simple Pandas App")
+    root.title("Movie Selection App")
 
     # create a frame widget
-    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe = ttk.Frame(root, padding="10 15 12 12", width=385, height=450)
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
+
+    # Style the buttons
+    ttk.Style().configure("TButton", padding=6, relief="flat", background="#ccc")
 
     # input variable has to be a StringVar, special var for Tkinter to grab user input
     selected = tk.StringVar()
 
     # create drop down menu
     # we have a function we execute on button click -- on_select from drop down menu
-    options = ttk.Combobox(mainframe, textvariable=selected)
+    dropdown_menu = ttk.Combobox(mainframe, textvariable=selected)
     # add a virtual event to call the "on_select" function when a new movie is selected
-    options.bind('<<ComboboxSelected>>', on_select)
+    dropdown_menu.bind("<<ComboboxSelected>>", on_select)
+    dropdown_menu.set("Select a Movie")
     # add movie names to dropdown
-    options['values'] = ['all'] + list(df['Title'].unique())
-    options.grid(column=1, row=1, padx=5, pady=15, sticky=W)
-
-    # Make a label for the dropdown menu
-    selected_entry = ttk.Label(mainframe, width=15, text='Select a Movie: ')
-    selected_entry.grid(column=0, row=1, padx=5, sticky=(E, W))
+    dropdown_menu["values"] = ["all"] + list(df["Title"].unique())
+    dropdown_menu.grid(row=0, column=0, padx=5, pady=15, sticky=W)
 
     # frame for table and button "Next Data"
     frame_data = tk.Frame(mainframe)
-    frame_data.grid(row=2, column=0, padx=5, pady=5)
+    frame_data.grid(row=1, column=0, padx=5)
 
     # button "Next Data" - inside "frame_data" - without showing it
-    next_button = tk.Button(frame_data, text="Next Data", command=next_data)
+    next_button = ttk.Button(frame_data, text="Next Data", command=next_data)
 
-    # table with data - inside "frame_data" - without showing it
-    table = tk.Frame(frame_data)
-    table.grid(row=0, column=0)
-
-    exit_button = tk.Button(mainframe, text="EXIT", command=root.destroy)
-    exit_button.grid(row=3, column=0, padx=5, pady=5, sticky=W)
+    # make an exit button to end the program
+    exit_button = ttk.Button(mainframe, text="EXIT", command=root.destroy)
+    exit_button.grid(row=2, column=0, padx=5, pady=5, sticky=W)
 
     root.mainloop()
