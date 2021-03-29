664352cf1f3f51c54e4c87453e3570ceae8a598b
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:37:33 -0400

    Write class

--- SIE507/venv/hw4/HousePurchase.py
+++ SIE507/venv/hw4/HousePurchase.py
@@ -2,5 +2,14 @@ from House import House
 from Purchase import Purchase
 
 class HousePurchase(House, Purchase):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    '''This static method calls the user prompts from the parent classes, House and Purchase'''
+    @staticmethod
+    def prompt_init():
+        args = House.prompt_init()
+        args.update(Purchase.prompt_init())
+        return args
+
+    '''This instance method calls the display methods from the parent classes, House and Purchase'''
+    def display(self):
+        House.display(self)
+        Purchase.display(self)
