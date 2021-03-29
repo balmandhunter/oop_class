762c97eedc569754459862938ee6bc350bdd5a90
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:38:50 -0400

    Error check user input

--- SIE507/venv/hw4/Rental.py
+++ SIE507/venv/hw4/Rental.py
@@ -5,19 +5,31 @@ class Rental:
         self.utilities = utilities
         self.rent = rent
 
-    '''This instance method prints rental information'''
+    '''This instance method prints the rent'''
     def display(self):
-        print('Furnished: ' + str(self.furnished) + '\n' +
-              'Utilities Included: ' + str(self.utilities) + '\n'
-              'Rent ($): ' + str(self.rent))
+        print('Transaction Type: Rental')
+        print('Rent ($): ' + str(self.rent))
 
-    '''This static method prompts the user for information about the rental and returns it, so that 
-    it can be passed as arguments to a new object'''
-    @staticmethod
-    def prompt_init():
-        # return {'furnished': input("Is the property furnished?:"),
-        #          'utilities': input("Does the rent include utilities?:"),
-        #          'rent': input("What is the amount of the rent?:")}
-        return {'furnished': True,
-                 'utilities': False,
-                 'rent': 1200}
+    '''This class method gets and validates user input for rental information.'''
+    @classmethod
+    def prompt_init(self):
+        furnished = input("Is the property furnished (yes or no)?:")
+        while furnished.lower() not in ['yes', 'no']:
+            print('Invalid Input')
+            furnished = input("Is the property furnished (yes or no)?:")
+
+        utilities = input("Does the rent include utilities (yes or no)?:")
+        while utilities.lower() not in ['yes', 'no']:
+            print('Invalid Input')
+            utilities = input("Does the rent include utilities (yes or no)?:")
+
+        rent = input("What is the amount of the rent?:")
+        while rent.isdigit() is False:
+            print('Invalid Input. Please enter an integer.')
+            rent = input("What is the amount of the rent?:")
+
+        info_dict = {'furnished': furnished,
+                      'utilities': utilities,
+                      'rent': rent}
+
+        return info_dict
