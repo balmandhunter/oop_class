1f0ffa271078d2f9bb9a175683c2b668ab58ec2b
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:37:56 -0400

    add method descriptions

--- SIE507/venv/hw4/HouseRental.py
+++ SIE507/venv/hw4/HouseRental.py
@@ -2,13 +2,14 @@ from House import House
 from Rental import Rental
 
 class HouseRental(House, Rental):
-    '''This static method '''
-    @staticmethod
-    def prompt_init(self):
+    '''This class method calls the user prompts from the parent classes, House and Rental'''
+    @classmethod
+    def prompt_init(cls):
         args = House.prompt_init()
         args.update(Rental.prompt_init())
         return args
 
+    '''This instance method calls the displat methods from the parent classes, house and rental'''
     def display(self):
         House.display(self)
         Rental.display(self)
