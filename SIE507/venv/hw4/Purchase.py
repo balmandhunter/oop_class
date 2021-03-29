b916681b5658f5e833932f2930bda348df125804
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:38:33 -0400

    error check user input

--- SIE507/venv/hw4/Purchase.py
+++ SIE507/venv/hw4/Purchase.py
@@ -4,13 +4,26 @@ class Purchase:
         self.price = price
         self.taxes = taxes
 
-    '''This instance method prints purchase information'''
+    '''This instance method prints the price of the property'''
     def display(self):
-        print('price: ' + str(self.price) + '\n' + 'taxes: ' + str(self.taxes))
+        print('Transaction Type: Purchase')
+        print('Price: ' + str(self.price))
 
     '''This static method prompts the user for information about the purchase and returns it, so that 
     it can be passed as arguments to a new object'''
     @staticmethod
-    def prompt_init(self):
-        return {'price': input("What is the listing price?: "),
-                'taxes': input("What are the property taxes?: ")}
+    def prompt_init():
+        price = input("What is the listing price?:")
+        while price.isdigit() is False:
+            print('Invalid Input. Please enter an integer.')
+            price = input("What is the listing price?:")
+
+        taxes =  input("What are the property taxes?:")
+        while taxes.isdigit() is False:
+            print('Invalid Input. Please enter an integer.')
+            taxes =  input("What are the property taxes?:")
+
+        info_dict =  {'price': price,
+                      'taxes': taxes}
+
+        return info_dict
