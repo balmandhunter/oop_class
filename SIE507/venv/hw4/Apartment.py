f45322d793d8791106a991ef39c319f74074a572
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:36:20 -0400

    get user input

--- SIE507/venv/hw4/Apartment.py
+++ SIE507/venv/hw4/Apartment.py
@@ -8,18 +8,21 @@ class Apartment(Property):
 
     '''This instance method prints information about the apartment'''
     def display(self):
-        # print property information from the parent class
-        Property.display()
-        # print apartment information
-        print('The apartment has a balcony: ' + str(self.balcony) + '\n' +
-              'The apartment has laundry facilities: ' + str(self.laundry))
+        print('-------------')
+        # print property address from the parent class
+        print('Adress: ' + Property.get_address(self))
+        # print the property type
+        print('Property Type: House')
+
 
     @staticmethod
     def prompt_init():
         # call the static method of the property class to prompt the user for arguments of the property class
         args = Property.prompt_init()
         # prompt the user to enter the arguments for an apartment
-        args.update({'balcony': input('Does the apartment have a balcony?: '),
-                     'laundry': input('Does the apartment have a washer and dryer?: ')})
+        # args.update({'balcony': input('Does the apartment have a balcony?: '),
+        #              'laundry': input('Does the apartment have a washer and dryer?: ')})
+        args.update({'balcony': 'Yes',
+                     'laundry': 'Yes'})
         # return the property and apartment arguments
         return args
