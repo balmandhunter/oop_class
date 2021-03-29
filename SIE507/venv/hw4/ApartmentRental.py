3bf26192c3655530f56d33b9306ceb1806c285d8
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-03-29 13:36:50 -0400

    write class

--- SIE507/venv/hw4/ApartmentRental.py
+++ SIE507/venv/hw4/ApartmentRental.py
@@ -2,5 +2,14 @@ from Apartment import Apartment
 from Rental import Rental
 
 class ApartmentRental(Apartment, Rental):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    '''This static method calls the user prompts from the parent classes, Apartment and Rental'''
+    @staticmethod
+    def prompt_init():
+        args = Apartment.prompt_init()
+        args.update(Rental.prompt_init())
+        return args
+
+    '''This instance method calls the display methods from the parent classes, Apartment and Rental'''
+    def display(self):
+        Apartment.display(self)
+        Rental.display(self)
