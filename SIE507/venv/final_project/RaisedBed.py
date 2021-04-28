14d46d874ab0e895501b4d0094ddc279aed10882
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-28 14:25:36 -0400

    add function to create a list of empty square objects

--- SIE507/venv/final_project/RaisedBed.py
+++ SIE507/venv/final_project/RaisedBed.py
@@ -1,5 +1,5 @@
 import numpy as np
-# from SquareFoot import Square
+from Square import Square
 
 class RaisedBed:
     def __init__(self, zone):
@@ -13,7 +13,17 @@ class RaisedBed:
     def print_plantype(self):
         print(self.plan_type)
 
+    '''Function to create a list of empty squares, based on the user input length and width'''
     def initialize_squares(self, length, width):
-        self.length = length
-        self.width = width
+        self.length = int(length)
+        self.width = int(width)
         print(self.length)
+
+        # create an empty list for seat objects
+        self.square_obj_list = [[0] * self.width for i in range(self.width)]
+
+        # loop through the rows and seats, and create a seat object for every seat, and save it in an array
+        for row in range(0, self.length):
+            for col in range(0, self.width):
+                self.square_obj_list[row][col] = Square(row, col)
+        print(self.square_obj_list)
