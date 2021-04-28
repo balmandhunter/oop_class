3c751276852854edcb35932b6ab25da4145fabbb
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-28 14:26:26 -0400

    create square class

---
+++ SIE507/venv/final_project/Square.py
@@ -0,0 +1,17 @@
+class Square:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        self.occupied = False
+        self.plant = None
+
+    def occupy_square(self, plant):
+        self.occupied = True
+        self.plant = plant
+
+    def empty_square(self):
+        self.occupied = False
+        self.plant = None
+
+    def return_plant(self):
+        return self.plant
