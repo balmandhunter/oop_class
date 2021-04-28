8c276de0dee0a4b18039b6276c3e1a0c48529052
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-27 20:36:29 -0400

    Change ZoneView class to ZonePlanSelectionView

--- SIE507/venv/final_project/View.py
+++ SIE507/venv/final_project/View.py
@@ -15,80 +15,53 @@ class View:
     def initialize_GUI(self):
         # setup and name the window
         self.root.title("Maine Garden Planner")
-        self.root.geometry("825x500")
+        self.root.geometry("300x300")
         # Create a frame widget
         self.mainframe = ttk.Frame(self.root)
         self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-
-
-class ZoneView(View):
-    def __init__(self, root, controller, **kwargs):
+class ZonePlanSelectionView(View):
+    def __init__(self, root, controller, dropdown_topic,  **kwargs):
         super().__init__(root, controller, **kwargs)
         self.root = root
         # initialize the view
         self.buttons = {}
         self.controller = controller
+        self.dropdown_topic = dropdown_topic
         super().initialize_GUI()
 
     def make_initial_view(self):
-        # input variable has to be a StringVar, special var for Tkinter to grab user input
-        # selected = tk.StringVar()
-
-        # Style the buttons
-        ttk.Style().configure("TButton", padding=6, relief="flat", background="#ccc")
-
-        # create drop down menu
-        # create the zone dropdown
-        zone_options = ['3','4','5','6']
-        zone_dropdown = self.c_create_dropdown("Select Your Gardening Zone", zone_options, 1, 0)
+        if self.dropdown_topic == 'view':
+            # create the zone dropdown
+            options = ['3','4','5','6']
+            title = 'Select Your Gardening Zone'
+        elif self.dropdown_topic == 'plan_type':
+            options = ['Load Last Year\'s Plan', 'Start a New Plan']
+            title = 'Select a Plan Type'
+        self.c_create_dropdown(title, options, 1, 0)
 
         # display the profession dropdown
-        zone_dropdown.grid(row=0, column=0, padx=15, pady=15)
+        self.zone_dropdown.grid(row=0, column=0, padx=15, pady=15)
 
         # make an exit button to end the program
         exit_button = ttk.Button(self.mainframe, text="EXIT", command=self.root.destroy)
         exit_button.grid(row=8, column=0, padx=5)
 
-        self.root.mainloop()
-
-
     def c_create_dropdown(self, title, values, buttonrow, buttoncol):
         # input variable has to be a StringVar, special var for Tkinter to grab user input
-        selected = tk.StringVar()
+        self.selected = tk.StringVar()
         # set default in dropdown
-        selected.set(title)
+        self.selected.set(title)
 
         # create drop down menu
-        options = tk.OptionMenu(self.mainframe, selected, *values)
+        self.zone_dropdown = tk.OptionMenu(self.mainframe, self.selected, *values)
+
         # make the button
         self.create_button(self.mainframe, 'Submit', buttonrow, buttoncol)
 
-        return options
-
     def create_button(self, frame, name, row, column):
-        self.buttons[name] = tk.Button(frame)
+        # Style the buttons
+        ttk.Style().configure("TButton", padding=6, relief="flat", background="#ccc")
+        self.buttons[name] = ttk.Button(frame)
         self.buttons[name]["text"] = name
         self.buttons[name].grid(row=row, column=column)
-
-    def on_select(val):
-        pass
-        # global df2
-        # global current_index
-        #
-        # val = selected.get()
-        # if val == "all":
-        #     df2 = df
-        #     next_button.grid_forget()
-        #     showdata()
-        # else:
-        #     df2 = df[df["Title"] == val]
-        #     # find index
-        #     current_index = 0
-        #     for ind in df.index:
-        #         if df["Title"][ind] == val:
-        #             current_index = ind
-        #
-        #     showdata()
-        #     # put next button on the canvas
-        #     next_button.grid(row=2, column=0, sticky=W)
