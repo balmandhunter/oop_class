c266633fe2e42fbe0f6d0b62c35ca9fce50b75cc
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-28 13:50:01 -0400

    Add bed size entry

--- SIE507/venv/final_project/Controller.py
+++ SIE507/venv/final_project/Controller.py
@@ -2,6 +2,8 @@ from View import View, ZonePlanSelectionView
 from PlantList import PlantList
 from RaisedBed import RaisedBed
 from abc import ABC, abstractmethod
+from tkinter import messagebox
+
 import tkinter as tk
 
 class Controller:
@@ -9,24 +11,56 @@ class Controller:
     def __init__(self, plant_list_filename, **kwargs):
         super().__init__(**kwargs)
         self.root = tk.Tk()
-        self.make_zone_selection_view()
+        self.make_selection_view('view')
         # self.plant_list_filename = plant_list_filename
 
     '''Create the initial GUI View, where gardeners select a Zone.'''
-    def make_zone_selection_view(self):
-        self.zoneview = ZonePlanSelectionView(self.root, self, 'view')
-        # Call the zoneview object to make the initial zoneview
-        self.zoneview.make_initial_view()
-        # Bind the button to the set_zone_and_update_view function
-        self.zoneview.buttons['Submit'].configure(command=self.set_zone_and_update_view)
+    def make_selection_view(self, dropdown_topic):
+        self.view = ZonePlanSelectionView(self.root, self, dropdown_topic)
+        # Call the view object to make the initial view
+        self.view.make_initial_view()
+
+        # set the command for the button
+        if dropdown_topic == 'view':
+            button_command = self.set_zone_and_update_view
+        elif dropdown_topic == 'plan_type':
+            button_command = self.get_and_send_plan_selection
+
+        # Bind the button to the appropriate function
+        self.view.buttons['Submit'].configure(command=button_command)
         self.root.mainloop()
 
-    '''Get the selected zone from the dropdown, send it to the raised bed and plant list,
-    and update the view.'''
+
+    '''Get the selected zone from the dropdown, send it to the plant list and raised bed, and update the view.'''
     def set_zone_and_update_view(self):
-        # Get the user-selected from the dropdown
-        self.zone = self.zoneview.selected.get()
+        # Get the user-selected zone from the dropdown
+        zone = self.view.selected.get()
         # Initialize a PlantList object with the zone
-        self.plantlist = PlantList('data/all_plants.csv', self.zone)
+        self.plantlist = PlantList('data/all_plants.csv', zone)
         # Initialize a raised bed with the zone
-        self.raisedbed = RaisedBed(self.zone)
+        self.raisedbed = RaisedBed(zone)
+        # Update the view to the plan selection
+        self.make_selection_view('plan_type')
+
+    '''Get button selection and send it to Raised Bed'''
+    def get_and_send_plan_selection(self):
+        # Get the user-selected plan type
+        plan_type = self.view.selected.get()
+        self.raisedbed.plan_type = plan_type
+        if plan_type == 'Start a New Plan':
+            print('Starting a New Plan')
+            self.view.make_size_view()
+            # Bind the button to the appropriate function
+            self.view.buttons['Submit'].configure(command=self.get_bed_size)
+            # self.root.mainloop()
+        else:
+            print('not coded yet')
+
+    def get_bed_size(self):
+        if self.view.length.get() == "" or self.view.length.get() == "":
+            messagebox.showinfo("Application Error", "Empty Textbox!")
+        ### add error message for non integer
+        else:
+            length = self.view.length.get()
+            width = self.view.width.get()
+        self.raisedbed.initialize_squares(length, width)
