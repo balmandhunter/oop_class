05945d18a1607bf64213b665e0b7f9e490d63070
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-27 20:34:31 -0400

    Retreive zone from dropdown and send to classes

--- SIE507/venv/final_project/Controller.py
+++ SIE507/venv/final_project/Controller.py
@@ -1,29 +1,32 @@
-from View import View, ZoneView
+from View import View, ZonePlanSelectionView
+from PlantList import PlantList
+from RaisedBed import RaisedBed
 from abc import ABC, abstractmethod
 import tkinter as tk
 
+class Controller:
 
-class Controller(ABC):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    #force all controllers to have a method to bind the button to an event
-    @abstractmethod
-    def bind(self):
-        raise NotImplementedError
-
-class ZoneController(Controller):
-
-    def __init__(self, **kwargs):
+    def __init__(self, plant_list_filename, **kwargs):
         super().__init__(**kwargs)
         self.root = tk.Tk()
-        self.zoneview = ZoneView(self.root, self)
-        self.zoneview.make_initial_view()
+        self.make_zone_selection_view()
+        # self.plant_list_filename = plant_list_filename
 
-    def bind(self):
+    '''Create the initial GUI View, where gardeners select a Zone.'''
+    def make_zone_selection_view(self):
+        self.zoneview = ZonePlanSelectionView(self.root, self, 'view')
+        # Call the zoneview object to make the initial zoneview
         self.zoneview.make_initial_view()
-        self.zoneview.buttons['Submit'].configure(command=self.set_zone)
+        # Bind the button to the set_zone_and_update_view function
+        self.zoneview.buttons['Submit'].configure(command=self.set_zone_and_update_view)
+        self.root.mainloop()
 
-    def set_zone(self):
-        self.zone = selected.get()
-        print(zone)
+    '''Get the selected zone from the dropdown, send it to the raised bed and plant list,
+    and update the view.'''
+    def set_zone_and_update_view(self):
+        # Get the user-selected from the dropdown
+        self.zone = self.zoneview.selected.get()
+        # Initialize a PlantList object with the zone
+        self.plantlist = PlantList('data/all_plants.csv', self.zone)
+        # Initialize a raised bed with the zone
+        self.raisedbed = RaisedBed(self.zone)
