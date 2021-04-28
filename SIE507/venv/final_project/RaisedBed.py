758ea33c5a3c831da4a1d1d3e711a8c993f6c84a
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-28 13:50:18 -0400

    add bed size

--- SIE507/venv/final_project/RaisedBed.py
+++ SIE507/venv/final_project/RaisedBed.py
@@ -4,8 +4,16 @@ import numpy as np
 class RaisedBed:
     def __init__(self, zone):
         self.zone = zone
-        self.print_zone()
+        self.plan_type = None
 
     def print_zone(self):
         print('Raised Bed Zone: ')
         print(self.zone)
+
+    def print_plantype(self):
+        print(self.plan_type)
+
+    def initialize_squares(self, length, width):
+        self.length = length
+        self.width = width
+        print(self.length)
