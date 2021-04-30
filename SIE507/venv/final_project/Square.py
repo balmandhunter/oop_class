021a3e97ceacee3850697b4dfa9b399288f969b1
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-30 14:45:11 -0400

    add plant object

--- SIE507/venv/final_project/Square.py
+++ SIE507/venv/final_project/Square.py
@@ -4,10 +4,12 @@ class Square:
         self.width = width
         self.occupied = False
         self.plant = '(empty)'
+        self.plant_object = None
 
-    def occupy_square(self, plant):
+    def occupy_square(self, plant, plant_object):
         self.occupied = True
         self.plant = plant
+        self.plant_object = plant_object
 
     def empty_square(self):
         self.occupied = False
