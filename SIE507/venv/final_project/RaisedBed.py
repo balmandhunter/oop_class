7b4447d80b0f2776f788c33b4467f546aa4c3e08
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-04-29 20:51:40 -0400

    Add creation of garden plan list

--- SIE507/venv/final_project/RaisedBed.py
+++ SIE507/venv/final_project/RaisedBed.py
@@ -25,13 +25,19 @@ class RaisedBed:
         for row in range(0, self.length):
             for col in range(0, self.width):
                 self.square_obj_list[row][col] = Square(row, col)
-        print(self.square_obj_list)
+
 
     '''Function to book a seat'''
     def fill_square(self, plant_row, plant_col, plant_name):
         # get square object
         square = self.square_obj_list[int(plant_row)-1][int(plant_col) - 1]
         square.occupy_square(plant_name)
-        # self.__updateSeatChart(Seat)
-        print(square.length, square.width, square.occupied, square.plant)
+
+    '''Create an array that can be saves to a csv from the square object list'''
+    def create_plan_from_square_list(self):
+        plant_location_list = [['row', 'column', 'occupied', 'plant']]
         print(self.square_obj_list)
+        # iterate through the square object list and append the name of the plant in each location to a list
+        for row_idx in range(0, self.length):
+            for col_idx in range(0, self.width):
+                plant_location_list.append([row_idx, col_idx, self.square_obj_list[row_idx][col_idx].occupied, self.square_obj_list[row_idx][col_idx].plant])
