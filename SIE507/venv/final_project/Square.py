9d70040d270dfd6e5dfe5a9d9cd8914cbda293f9
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-05-03 11:55:19 -0400

    add return_plant_name

--- SIE507/venv/final_project/Square.py
+++ SIE507/venv/final_project/Square.py
@@ -3,17 +3,18 @@ class Square:
         self.length = length
         self.width = width
         self.occupied = False
-        self.plant = '(empty)'
+        self.plant_name = '(empty)'
         self.plant_object = None
 
     def occupy_square(self, plant, plant_object):
         self.occupied = True
         self.plant = plant
         self.plant_object = plant_object
+        self.plant_name = plant_object.plant_name
 
     def empty_square(self):
         self.occupied = False
         self.plant = None
 
-    def return_plant(self):
-        return self.plant
+    def return_plant_name(self):
+        return self.plant_name
