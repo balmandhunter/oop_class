cc90fd5d36ff218e06b54af9d2c4709b35d60d36
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-05-05 19:53:33 -0400

    add method descriptions

--- SIE507/venv/final_project/Square.py
+++ SIE507/venv/final_project/Square.py
@@ -6,15 +6,18 @@ class Square:
         self.plant_name = '(empty)'
         self.plant_object = None
 
+    '''Add a plant to the square'''
     def occupy_square(self, plant, plant_object):
         self.occupied = True
         self.plant = plant
         self.plant_object = plant_object
         self.plant_name = plant_object.plant_name
 
+    '''Remove a plant from the square'''
     def empty_square(self):
         self.occupied = False
         self.plant = None
 
+    '''Return the name of the plant'''
     def return_plant_name(self):
         return self.plant_name
