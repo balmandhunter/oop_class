fe3e2f17cad8a50408832699f4f8c965c6eadd93
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-05-05 19:52:39 -0400

    make exception for size entry

--- SIE507/venv/final_project/Exceptions.py
+++ SIE507/venv/final_project/Exceptions.py
@@ -4,14 +4,14 @@ class GardenExceptions():
     def __init__(self):
         pass
 
+    '''Make a callback to the controller so the class can access it'''
     def call_back(self, controller):
         self.controller = controller
 
+    '''Check user entry for adding a new plant'''
     def check_plant_location_entry(self, plant_row, plant_col, plant_name):
-        # make sure the length and width entries are integers
-        while plant_row.isdigit() is False or plant_col.isdigit() is False:
-            retry = messagebox.askretrycancel(title=None, message="Please enter an integer!")
-            self.controller.get_plan()
+        # make sure the entries are integers
+        self.check_row_col_entry(plant_row, plant_col)
         # make sure the entry is within the size range of the raised bed
         while int(plant_row) not in range(1, int(self.controller.length) + 1):
             retry = messagebox.askretrycancel(title=None, message="Row out of range!")
@@ -22,4 +22,11 @@ class GardenExceptions():
         # Make sure the user selected a plant
         while plant_name == "Select a Plant":
             retry = messagebox.askretrycancel(title=None, message="No plant selected!")
+            self.controller.get_plan()
+
+    '''Check user entry for adding a number of rows and columns'''
+    def check_row_col_entry(self, plant_row, plant_col):
+        # make sure the length and width entries are integers
+        while plant_row.isdigit() is False or plant_col.isdigit() is False:
+            retry = messagebox.askretrycancel(title=None, message="Please enter an integer!")
             self.controller.get_plan()
