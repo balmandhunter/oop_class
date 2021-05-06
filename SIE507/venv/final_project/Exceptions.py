0a1de143dd81b24daf2d4aca030ce3209c1bfb49
balmandhunter <berkeleyalmandhunter@gmail.com>
2021-05-05 22:42:33 -0400

    redo exceptions

--- SIE507/venv/final_project/Exceptions.py
+++ SIE507/venv/final_project/Exceptions.py
@@ -13,20 +13,16 @@ class GardenExceptions():
         # make sure the entries are integers
         self.check_row_col_entry(plant_row, plant_col)
         # make sure the entry is within the size range of the raised bed
-        while int(plant_row) not in range(1, int(self.controller.length) + 1):
-            retry = messagebox.askretrycancel(title=None, message="Row out of range!")
-            self.controller.get_plan()
-        while int(plant_col) not in range(1, int(self.controller.width) + 1):
-            retry = messagebox.askretrycancel(title=None, message="Column out of range!")
-            self.controller.get_plan()
+        if int(plant_row) not in range(1, int(self.controller.length) + 1):
+            messagebox.showwarning(title=None, message="Row out of range! Please try again.")
+        if int(plant_col) not in range(1, int(self.controller.width) + 1):
+            messagebox.showwarning(title=None, message="Column out of range! Please try again.")
         # Make sure the user selected a plant
-        while plant_name == "Select a Plant":
-            retry = messagebox.askretrycancel(title=None, message="No plant selected!")
-            self.controller.get_plan()
+        if plant_name == "Please Select a Plant":
+            messagebox.showwarning(title=None, message="No plant selected! Please select a plant.")
 
     '''Check user entry for adding a number of rows and columns'''
     def check_row_col_entry(self, plant_row, plant_col):
         # make sure the length and width entries are integers
-        while plant_row.isdigit() is False or plant_col.isdigit() is False:
-            retry = messagebox.askretrycancel(title=None, message="Please enter an integer!")
-            self.controller.get_plan()
+        if plant_row.isdigit() is False or plant_col.isdigit() is False:
+            messagebox.showwarning(title=None, message="Please enter an integer!")
